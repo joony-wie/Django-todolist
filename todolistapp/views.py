f38bcfd03d3fdacb4c7e1de8c3a794b169e2cbd2
“encia200” <encia200@gmail.com>
2018-11-02 23:53:02 +0900

    Modified templates to display todos from database.

--- todolistapp/views.py
+++ todolistapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Todo
+
+def todo_list(request):
+    todos = Todo.objects.order_by('deadline')
+    context = {'todos': todos}
+    return render(request, 'todolistapp/todo_list.html', context)
 
 # Create your views here.
